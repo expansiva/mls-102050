7be1705ff24cf376bbe34e192d78a2c6d1ad07f3
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-07-05 15:56:13 -0300

    group-1
    group-1

---
+++ l2/cafeFlow/web/desktop/page11/aiPromotionSuggestions.test.ts
@@ -0,0 +1,2 @@
+/// <mls fileReference="_102050_/l2/cafeFlow/web/desktop/page11/aiPromotionSuggestions.test.ts" enhancement="_blank"/>
+
---
+++ l2/cafeFlow/web/desktop/page11/manageMenuItems.test.ts
@@ -0,0 +1,2 @@
+/// <mls fileReference="_102050_/l2/cafeFlow/web/desktop/page11/manageMenuItems.test.ts" enhancement="_blank"/>
+
